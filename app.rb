ef7b1066cd1bd5cf9dd4e7614abf4fd93438e9f9
Kornelis Sietsma <korny@sietsma.com>
2010-04-07 10:10:55 +1000

    initial big refactor - reverted to original css, html files with a bit of erb, made css on-the-fly with ruby code for font-face and per-font styles, added main page for overall font index

--- app.rb
+++ app.rb
@@ -1,17 +1,147 @@
 require 'rubygems'
 require 'sinatra'
-require 'haml'
-require 'sass'
+require 'erb'
 
-get '/stylesheets/:name.css' do
+FONT_DIR = File.join(File.dirname(__FILE__),'public','fonts')
+
+helpers do
+  def font_dir(name, style = nil)
+    if style
+      File.join(FONT_DIR,name,style)
+    else
+      File.join(FONT_DIR,name)
+    end
+  end
+  def valid_font?(name)
+    File.exists?(font_dir(name))
+  end
+  def valid_font_style?(name, style)
+    File.exists?(font_dir(name,style))
+  end
+  def all_fonts
+    Dir.new(FONT_DIR).select { |dir| dir =~ /^[^\.]/ && File.directory?(File.join(FONT_DIR,dir)) }.sort
+  end
+  def font_styles(basename)
+    $stderr.puts "looking for font dirs in #{font_dir(basename)}"
+    results = Dir.new(font_dir(basename)).select { |dir| puts dir; dir =~ /^[^\.]/ && File.directory?(File.join(font_dir(basename),dir)) }.sort
+    $stderr.puts "results: #{results.inspect}"
+    results
+  end
+  def find_fontfile(path, extension)
+    files = Dir.glob(path + "/*." + extension)
+    raise "More than one #{extension} file in #{path}" if files.size > 1
+    files.size == 0 ? nil : File.basename(files[0])
+  end
+  def single_font(name, path, prefix)
+    $stderr.puts "producing css for '#{name}' in path #{path} with prefix #{prefix}"
+    eot_file = find_fontfile(path,'eot')
+    eot_css = eot_file ? "src: url('#{prefix}#{eot_file}');" : ''
+    local_css = "local('no_such_local_name_we_hope')"
+    woff_file = find_fontfile(path,'woff')
+    woff_css = woff_file ? "url('#{prefix}#{woff_file}') format(\"woff\")" : ''
+    otf_file = find_fontfile(path,'otf')
+    otf_css = otf_file ? "url('#{prefix}#{otf_file}') format(\"opentype\")" : ''
+    ttf_file = find_fontfile(path,'ttf')
+    ttf_css = ttf_file ? "url('#{prefix}#{ttf_file}') format(\"truetype\")" : ''
+    svg_file = find_fontfile(path,'svg')
+    svg_suffix = name  # TODO: better svn id detection???
+    svg_css = svg_file ? "url('#{prefix}#{svg_file}##{svg_suffix}') format(\"svg\")" : ''
+
+    combo_css = [local_css,woff_css,otf_css,ttf_css,svg_css].reject{|txt| txt.empty?}.join(", ")
+    css = <<-EOT
+@font-face {
+   font-family: '#{name}';
+   #{eot_css}
+   src: #{combo_css};
+}
+    EOT
+  end
+  def font_family_css(name)
+    # returns the css for the fonts, plus a list of styles found
+    raise "invalid font #{name}" unless valid_font? name
+    all_css = single_font(name,font_dir(name),"/fonts/#{name}/") + "\n"
+    styles = {}
+    font_styles(name).each do |style|
+      $stderr.puts "processing name #{name} style #{style}"
+      familyname = "#{name}#{style}"
+      all_css += single_font(familyname,font_dir(name,style),"/fonts/#{name}/#{style}/") + "\n"
+      styles[style] = familyname
+    end
+    {:css => all_css, :styles => styles}
+  end
+end
+
+get '/stylesheets/fonts/all.css' do
   content_type 'text/css', :charset => 'utf-8'
-  sass :"stylesheets/#{params[:name]}"
+  all_fonts.collect {|font| font_family_css(font)[:css]}.join("\n")
+end
+
+get '/stylesheets/fonts/:basefont.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  basefont = params[:basefont]
+  raise "Bad font name '#{basefont}'" unless valid_font? basefont
+  font_family_css(basefont)[:css]
+end
+
+get '/stylesheets/:basefont/details.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  basefont = params[:basefont]
+  raise "Bad font name '#{basefont}'" unless valid_font? basefont
+  fontinfo = font_family_css(basefont)
+  styles = fontinfo[:styles]
+  output = "body, .typeface1 { font-family: \"#{basefont}\", Georgia; }\n"
+  if styles['Italic']
+    output += "body em, .typeface1 em { font-family: \"#{styles['Italic']}\", Georgia; }\n"
+  end
+  if styles['Bold']
+    output += "body strong, .typeface1 strong { font-family: \"#{styles['Bold']}\", Georgia; }\n"
+  end
+
+  if styles['BoldItalic']
+    output += "body strong em, .typeface1 strong em { font-family: \"#{styles['BoldItalic']}\", Georgia; }\n"
+    output += "body em strong, .typeface1 em strong { font-family: \"#{styles['BoldItalic']}\", Georgia; }\n"
+  end
+  output += ".typeface2 { font-family: Verdana; }\n"
+  output
+end
+
+get '/stylesheets/main_fonts.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  output = ""
+  all_fonts.each do |font|
+    output += ".#{font} { font-family: \"#{font}\", Georgia; }\n"
+    styles = font_family_css(font)[:styles]
+    styles.values.each do |style|
+      output += ".#{style} { font-family:\"#{style}\", Georgia; }\n"
+    end
+  end
+  output
 end
 
 get '/' do
-  haml :index
+  @fonts = []
+  all_fonts.each do |font|
+    styles = font_family_css(font)[:styles]
+    @fonts << {:font => font, :children => styles.values}
+  end
+  erb :index
 end
 
-get '/:view' do
-  haml :"#{params[:view]}"
+get '/details/:basefont' do
+  @basefont = params[:basefont]
+  raise "Bad font name '#{@basefont}'" unless valid_font? @basefont
+  erb :details
 end
+
+#get '/stylesheets/:name.css' do
+#  content_type 'text/css', :charset => 'utf-8'
+#  less :"stylesheets/#{params[:name]}"
+#end
+
+#get '/' do
+#  haml :index
+#end
+
+#get '/:view' do
+#  haml :"#{params[:view]}"
+#end
