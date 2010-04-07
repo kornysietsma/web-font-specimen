68fd1fc790d47f0da876eb5c08b89544a6dbaf74
Kornelis Sietsma <korny@sietsma.com>
2010-04-07 22:22:31 +1000

    big refactor to use helper class instead of hacky functions

--- app.rb
+++ app.rb
@@ -1,164 +1,64 @@
 require 'rubygems'
 require 'sinatra'
 require 'erb'
+require 'lib/fontinfo'
 
 FONT_DIR = File.join(File.dirname(__FILE__),'public','fonts')
 
 helpers do
-  def font_dir(name, style = nil)
-    if style
-      File.join(FONT_DIR,name,style)
-    else
-      File.join(FONT_DIR,name)
-    end
-  end
-  def valid_font?(name)
-    File.exists?(font_dir(name))
-  end
-  def valid_font_style?(name, style)
-    File.exists?(font_dir(name,style))
-  end
-  def all_fonts
-    Dir.new(FONT_DIR).select { |dir| dir =~ /^[^\.]/ && File.directory?(File.join(FONT_DIR,dir)) }.sort
-  end
-  def font_styles(basename)
-    $stderr.puts "looking for font dirs in #{font_dir(basename)}"
-    results = Dir.new(font_dir(basename)).select { |dir| puts dir; dir =~ /^[^\.]/ && File.directory?(File.join(font_dir(basename),dir)) }.sort
-    $stderr.puts "results: #{results.inspect}"
-    results
-  end
-  def find_fontfile(path, extension)
-    files = Dir.glob(path + "/*." + extension)
-    raise "More than one #{extension} file in #{path}" if files.size > 1
-    files.size == 0 ? nil : File.basename(files[0])
-  end
-  def single_font_css(name, path, prefix)
-    $stderr.puts "producing css for '#{name}' in path #{path} with prefix #{prefix}"
-    eot_file = find_fontfile(path,'eot')
-    eot_css = eot_file ? "src: url('#{prefix}#{eot_file}');" : ''
-    local_css = "local('no_such_local_name_we_hope')"
-    woff_file = find_fontfile(path,'woff')
-    woff_css = woff_file ? "url('#{prefix}#{woff_file}') format(\"woff\")" : ''
-    otf_file = find_fontfile(path,'otf')
-    otf_css = otf_file ? "url('#{prefix}#{otf_file}') format(\"opentype\")" : ''
-    ttf_file = find_fontfile(path,'ttf')
-    ttf_css = ttf_file ? "url('#{prefix}#{ttf_file}') format(\"truetype\")" : ''
-    svg_file = find_fontfile(path,'svg')
-    svg_suffix = name  # TODO: better svn id detection???
-    svg_css = svg_file ? "url('#{prefix}#{svg_file}##{svg_suffix}') format(\"svg\")" : ''
-
-    combo_css = [local_css,woff_css,otf_css,ttf_css,svg_css].reject{|txt| txt.empty?}.join(", ")
-    css = <<-EOT
-@font-face {
-   font-family: '#{name}';
-   #{eot_css}
-   src: #{combo_css};
-}
-    EOT
-  end
-
-  def font_style_css(name, style)
-    raise "Bad font name '#{name}' style '#{style}'" unless valid_font_style? name, style
-    familyname = "#{name}#{style}"
-    single_font_css(familyname, font_dir(name,style),"/fonts/#{name}/#{style}/")
-  end
-
-  def font_family_css(name)
-    # returns the css for the fonts, plus a list of styles found
-    raise "invalid font #{name}" unless valid_font? name
-    all_css = single_font_css(name,font_dir(name),"/fonts/#{name}/") + "\n"
-    styles = {}
-    font_styles(name).each do |style|
-      $stderr.puts "processing name #{name} style #{style}"
-      familyname = "#{name}#{style}"
-      all_css += font_style_css(name,style) + "\n"
-      styles[style] = familyname
-    end
-    {:css => all_css, :styles => styles}
+  def fonts
+    # cache just in case - though it'll be re-generated on each request, of course:
+    @fonts ||= FontDir.new(FONT_DIR,"/")
   end
 end
 
 get '/stylesheets/fonts/all.css' do
   content_type 'text/css', :charset => 'utf-8'
-  all_fonts.collect {|font| font_family_css(font)[:css]}.join("\n")
+  fonts.all_css
 end
 
-get '/stylesheets/fonts/:basefont.css' do
+get %r{/stylesheets/fonts/([\w_\-/]+).css} do
   content_type 'text/css', :charset => 'utf-8'
-  basefont = params[:basefont]
-  raise "Bad font name '#{basefont}'" unless valid_font? basefont
-  font_family_css(basefont)[:css]
+  font_path = params[:captures].first
+  fonts.font_by_path(font_path).all_css
 end
 
-get '/stylesheets/:basefont/details.css' do
+get %r{/stylesheets/details/([\w_\-/]+).css} do
   content_type 'text/css', :charset => 'utf-8'
-  basefont = params[:basefont]
-  raise "Bad font name '#{basefont}'" unless valid_font? basefont
-  fontinfo = font_family_css(basefont)
-  styles = fontinfo[:styles]
-  output = "body, .typeface1 { font-family: \"#{basefont}\", Georgia; }\n"
-  if styles['Italic']
-    output += "body em, .typeface1 em { font-family: \"#{styles['Italic']}\", Georgia; }\n"
+  font_path = params[:captures].first
+  font = fonts.font_by_path(font_path)
+  output = "body, .typeface1 { font-family: \"#{font.familyname}\", Georgia; }\n"
+  if font.basefont?
+    if font.styles['Italic']
+      output += "body em, .typeface1 em { font-family: \"#{font.styles['Italic'].familyname}\", Georgia; }\n"
+    end
+    if font.styles['Bold']
+      output += "body strong, .typeface1 strong { font-family: \"#{font.styles['Bold'].familyname}\", Georgia; }\n"
+    end
+    if font.styles['BoldItalic']
+      output += "body strong em, .typeface1 strong em { font-family: \"#{font.styles['BoldItalic'].familyname}\", Georgia; }\n"
+      output += "body em strong, .typeface1 em strong { font-family: \"#{font.styles['BoldItalic'].familyname}\", Georgia; }\n"
+    end
   end
-  if styles['Bold']
-    output += "body strong, .typeface1 strong { font-family: \"#{styles['Bold']}\", Georgia; }\n"
-  end
-
-  if styles['BoldItalic']
-    output += "body strong em, .typeface1 strong em { font-family: \"#{styles['BoldItalic']}\", Georgia; }\n"
-    output += "body em strong, .typeface1 em strong { font-family: \"#{styles['BoldItalic']}\", Georgia; }\n"
-  end
-  output += ".typeface2 { font-family: Verdana; }\n"
-  output
-end
-
-get '/stylesheets/:basefont/:style/details.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  basefont = params[:basefont]
-  style = params[:style]
-  raise "Bad font name '#{basefont}' style '#{style}'" unless valid_font_style? basefont, style
-  fontinfo = font_family_css(basefont)
-  stylefamily = fontinfo[:styles][style]
-  output = "body, .typeface1 { font-family: \"#{stylefamily}\", Georgia; }\n"
   output += ".typeface2 { font-family: Verdana; }\n"
   output
 end
 
 get '/stylesheets/main_fonts.css' do
   content_type 'text/css', :charset => 'utf-8'
-  output = ""
-  all_fonts.each do |font|
-    output += ".#{font} { font-family: \"#{font}\", Georgia; }\n"
-    styles = font_family_css(font)[:styles]
-    styles.values.each do |style|
-      output += ".#{style} { font-family:\"#{style}\", Georgia; }\n"
-    end
-  end
-  output
+  fonts.all_families.collect do |family|
+    ".#{family} { font-family: \"#{family}\", Georgia; }\n"
+  end.join("\n")
 end
 
 get '/' do
-  fonts = []
-  all_fonts.each do |font|
-    styles = font_family_css(font)[:styles]
-    children = styles.collect { |key, value| {:style => key, :family => value} }
-    fonts << {:font => font, :children => children }
-  end
   erb :index, :locals => {:fonts => fonts, :sample => "Handglove 123"}
 end
 
-get '/details/:basefont' do
-  basefont = params[:basefont]
-  raise "Bad font name '#{basefont}'" unless valid_font? basefont
-  erb :details, :locals => {:basefont => basefont, :css_path => basefont, :fontname => basefont}
-end
-
-get '/details/:basefont/:style' do
-  basefont = params[:basefont]
-  style = params[:style]
-  raise "Bad font name '#{basefont}' style '#{style}'" unless valid_font_style? basefont, style
-  css_path = "#{basefont}/#{style}"
-  fontname = "#{basefont} #{style}"
-  erb :details, :locals => {:basefont => basefont, :css_path => css_path, :fontname => fontname}
+get %r{/details/([\w_\-/]+)} do
+  $stderr.puts params.inspect
+  font_path = params[:captures].first
+  font = fonts.font_by_path(font_path)
+  erb :details, :locals => {:basefont => font.familyname, :css_path => font.url_base[0..-2], :fontname => font.familyname}
 end
 
---
+++ lib/fontinfo.rb
@@ -0,0 +1,146 @@
+class FontDir
+  FONT_FILE_PATTERN = "*.{eot,woff,ttf,otf}"
+  attr_accessor :name
+  def initialize(font_path, url_base, name = nil)
+    @name = name  # the name of the path under it's parent, or nil for the root
+    @font_path = font_path
+    @url_base = url_base
+    @children = []  # FontDir entries for child directories
+    @fonts = [] # Font entries for direct child fonts
+    find_children
+  end
+  
+  def find_children
+    Dir.new(@font_path).each do |filename|
+      next if filename =~ /\.\.?/
+      child_path = File.join(@font_path, filename)
+      next unless File.directory? child_path
+      child_url = "#{@url_base}#{filename}/"
+      unless Dir.glob(File.join(child_path,FONT_FILE_PATTERN)).empty?
+        @fonts << Font.new(child_path, child_url, filename)
+      else
+        child = FontDir.new(child_path, child_url, filename)
+        @children << child unless child.empty?
+      end
+    end
+  end
+  def empty?
+    @fonts.empty? && @children.empty?
+  end
+  def all_css
+    @fonts.collect {|font| font.all_css}.join("\n") + \
+      @children.collect {|child| child.all_css}.join("\n")
+  end
+  def all_families
+    (@fonts.collect {|font| font.all_families} + \
+      @children.collect {|child| child.all_families}).flatten
+  end
+  def font_by_path(subpath)
+    $stderr.puts "looking for font by path: '#{subpath}'"
+    # note subpath starts with no '/', and it's a child of current path
+    first, rest = subpath.split('/',2)
+    if rest
+      font_match = @fonts.detect {|font| font.family == first}
+      return font_match.style_by_path(rest) if font_match
+      dir_match = @children.detect {|child| child.name == first}
+      raise "can't find valid #{first} under #{@font_path}" unless dir_match
+      return dir_match.font_by_path(rest)
+    else
+      font_match = @fonts.detect {|font| font.family == first}
+      raise "can't find font #{first} under #{@font_path}" unless font_match
+      return font_match
+    end
+  end
+
+  def html_index
+    # assumes we are already in a parent's 'ul' list
+    @children.collect {|child| "<li>\n#{child.name} :<ul>\n" + child.html_index + "</ul>\n</li>\n"}.join + \
+      @fonts.collect {|font| "<li>\n" + font.html_index + "</li>\n"}.join
+  end
+end
+
+class Font
+  attr_accessor :styles, :style, :family, :url_base
+  def initialize(font_path, url_base, family, style = nil)
+    $stderr.puts "initializing font with path '#{font_path}', url '#{url_base}',family '#{family}'"
+    raise "Invalid Font directory #{font_path}" if Dir.glob(File.join(font_path,FontDir::FONT_FILE_PATTERN)).empty?
+    @font_path = font_path
+    @url_base = url_base
+    @family = family
+    @style = style
+    @styles = style.nil? ? find_styles : {}
+  end
+  def basefont?
+    @style.nil?
+  end
+  def familyname
+    @family + (@style || '')
+  end
+  def find_styles
+    styles = {}
+    Dir.new(@font_path).each do |filename|
+      next if filename =~ /\.\.?/
+      child_path = File.join(@font_path, filename)
+      next unless File.directory? child_path
+      child_url = "#{@url_base}#{filename}/"
+      unless Dir.glob(File.join(child_path,FontDir::FONT_FILE_PATTERN)).empty?
+        styles[filename] = Font.new(child_path, child_url, @family, filename)
+      # no 'else' - if it's not a font dir, we look no deeper
+      end
+    end
+    styles
+  end
+  def self.find_fontfile(path, extension)
+    files = Dir.glob(path + "/*." + extension)
+    raise "More than one #{extension} file in #{path}" if files.size > 1
+    files.size == 0 ? nil : File.basename(files[0])
+  end
+  def css
+    eot_file = Font.find_fontfile(@font_path,'eot')
+    eot_css = eot_file ? "src: url('/fonts#{@url_base}#{eot_file}');" : ''
+    local_css = "local('#{familyname}')"
+    woff_file = Font.find_fontfile(@font_path,'woff')
+    woff_css = woff_file ? "url('/fonts#{@url_base}#{woff_file}') format(\"woff\")" : ''
+    otf_file = Font.find_fontfile(@font_path,'otf')
+    otf_css = otf_file ? "url('/fonts#{@url_base}#{otf_file}') format(\"opentype\")" : ''
+    ttf_file = Font.find_fontfile(@font_path,'ttf')
+    ttf_css = ttf_file ? "url('/fonts#{@url_base}#{ttf_file}') format(\"truetype\")" : ''
+    svg_file = Font.find_fontfile(@font_path,'svg')
+    svg_suffix = familyname  # TODO: better svn id detection???
+    svg_css = svg_file ? "url('/fonts#{@url_base}#{svg_file}##{svg_suffix}') format(\"svg\")" : ''
+
+    combo_css = [local_css,woff_css,otf_css,ttf_css,svg_css].reject{|txt| txt.empty?}.join(", ")
+    <<-EOT
+@font-face {
+   font-family: '#{familyname}';
+   #{eot_css}
+   src: #{combo_css};
+}
+    EOT
+  end
+  def all_css
+    css + "\n" + @styles.values.collect {|style| style.css}.join("\n")
+  end
+  def style_by_path(name)
+    raise "No valid style #{style} in #{@font_path}" unless @styles.has_key? name
+    @styles[name]
+  end
+  def all_families
+    if basefont?
+      [familyname] + @styles.values.collect {|style| style.familyname}
+    else
+      familyname
+    end
+  end
+  def own_html_index
+    %Q{<a href="/details#{@url_base[0..-2]}">#{familyname} : <span class="#{familyname}">Lorem Ipsum</span></a>\n}
+  end
+  def html_index
+    # assume inside a parent's li element
+    if !basefont?
+      own_html_index
+    else
+      own_html_index + "<ul>\n" + @styles.values.collect{|style| "<li>" + style.html_index + "</li>"}.join("\n") + "\n</ul>\n"
+    end
+  end
+end
